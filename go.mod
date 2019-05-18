01d766962960f0f1a72844870206357d5b0f52f4
Denis Zinovyev <zinoviev.denis@yandex.ru>
2019-05-19 01:10:49 +0300

    logs format update

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	bou.ke/monkey v1.0.1
 	github.com/mailru/easyjson v0.0.0-20190403194419-1ea4449da983
 	github.com/streadway/amqp v0.0.0-20190404075320-75d898a42a94
-	github.com/studtool/common v0.0.0-20190518195043-e1b27f5c60c5
+	github.com/studtool/common v0.0.0-20190518220330-3a0ba4eb3aa3
 	github.com/valyala/fasttemplate v1.0.1
 	go.uber.org/dig v1.7.1-0.20190327003609-4cc9f8898fe3
 )
--- vendor/github.com/studtool/common/logs/logger.go
+++ vendor/github.com/studtool/common/logs/logger.go
@@ -1,6 +1,10 @@
 package logs
 
 import (
+	"fmt"
+	"os"
+	"runtime"
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -19,21 +23,62 @@ func NewLogger() *Logger {
 }
 
 func (log *Logger) Debug(args ...interface{}) {
-	log.logger.Debug(args...)
+	log.logger.WithFields(log.callerInfo()).Debug(args...)
 }
 
 func (log *Logger) Info(args ...interface{}) {
-	log.logger.Info(args...)
+	log.logger.WithFields(log.callerInfo()).Info(args...)
 }
 
 func (log *Logger) Warning(args ...interface{}) {
-	log.logger.Warn(args...)
+	log.logger.WithFields(log.callerInfo()).Warn(args...)
 }
 
 func (log *Logger) Error(args ...interface{}) {
-	log.logger.Error(args...)
+	log.logger.WithFields(log.callerInfo()).Error(args...)
 }
 
 func (log *Logger) Fatal(args ...interface{}) {
-	log.logger.Fatal(args...)
+	log.logger.WithFields(log.callerInfo()).Fatal(args...)
+}
+
+const (
+	callerStackDepth = 3
+)
+
+var (
+	host = getHostname()
+	pid  = int64(os.Getpid())
+)
+
+func (log *Logger) callerInfo() logrus.Fields {
+	fpcs := make([]uintptr, 1)
+
+	n := runtime.Callers(callerStackDepth, fpcs)
+	if n == 0 {
+		return nil
+	}
+
+	caller := runtime.FuncForPC(fpcs[0] - 1)
+	if caller == nil {
+		return nil
+	}
+
+	name := caller.Name()
+	file, line := caller.FileLine(fpcs[0] - 1)
+
+	return logrus.Fields{
+		"host": host,
+		"pid":  pid,
+		"func": name,
+		"file": fmt.Sprintf("%s:%d", file, line),
+	}
+}
+
+func getHostname() string {
+	h, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	return h
 }
